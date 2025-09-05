99da18c7461c32866ce723e7717848fbf2aae50c
wmarbar <wmartinez0.01666@gmail.com>
2025-09-04 21:48:01 -0500

    Tercer commit: se carga audio y pagina de botones

--- app.py
+++ app.py
@@ -4,8 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # Puedes cambiar el título dinámicamente si quieres
     return render_template("index.html", titulo="8 de septiembre")
+
+@app.route("/comenzar")
+def landing():
+    return render_template("landing.html")
+
+@app.route("/que-paso-8-septiembre")
+def que_paso():
+    return "<h1 style='font-family: system-ui; text-align:center; margin-top:3rem;'>Timeline en construcción…</h1>"
+
+@app.route("/coincidiendo-contigo")
+def coincidiendo():
+    return "<h1 style='font-family: system-ui; text-align:center; margin-top:3rem;'>Coincidiendo contigo — próximamente</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
